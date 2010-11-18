ee4ff17cff285e756771f10ded77d5cb27e48591
Craig H Anderson <crai@coot.net>
2010-11-18 07:40:36 -0700

    Animation is working

--- conway_game_document_ready.js
+++ conway_game_document_ready.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  GAME_OF_LIFE.board = GAME_OF_LIFE.game_board_constructor({});
+  GAME_OF_LIFE.board = GAME_OF_LIFE.game_board_constructor({row_count: 40, col_count:40});
   GAME_OF_LIFE.board.create_board();
   GAME_OF_LIFE.ui = GAME_OF_LIFE.ui_constructor({});
 });
